7f18c2bc2c5a4cdec96efd57a35a2a287977f420
Loay Hamod <112626695+LoayHamod@users.noreply.github.com>
2023-12-01 17:25:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require('cookie-session');
 const cors = require("cors");
-const mongoose = requite("mongoose");
+const mongoose = require("mongoose");
   mongoose.Promise = global.Promise;
 
 const dbConfig = require("./app/config/db.config");
